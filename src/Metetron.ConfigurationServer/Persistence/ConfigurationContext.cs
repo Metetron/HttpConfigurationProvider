54094181b71cea1ede084d1a470eb29a7b25128e
Daniel Freydl <daniel.freydl@tdk-electronics.tdk.com>
2020-10-12 14:11:39 +0200

    change target framework and add entities to context

--- src/Metetron.ConfigurationServer/Persistence/ConfigurationContext.cs
+++ src/Metetron.ConfigurationServer/Persistence/ConfigurationContext.cs
@@ -1,10 +1,15 @@
 using System.Reflection;
+using Metetron.ConfigurationServer.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace Metetron.ConfigurationServer.Persistence
 {
     public class ConfigurationContext : DbContext
     {
+        public DbSet<App> Apps { get; set; } = null!;
+        public DbSet<AppConfiguration> AppConfigurations { get; set; } = null!;
+        public DbSet<AppSetting> AppSettings { get; set; } = null!;
+
         public ConfigurationContext(DbContextOptions<ConfigurationContext> options) : base(options) { }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
